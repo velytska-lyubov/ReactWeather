ba05dde4b2c77a0af08a04f49f16ced0d96bbc09
Lyubov Velytska <velytska.lyubov@gmail.com>
2017-01-24 15:26:55 +0100

    Navigation changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
 
 
-
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+
   )
 };
 
