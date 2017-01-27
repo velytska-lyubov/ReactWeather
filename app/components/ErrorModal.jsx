ebf39082d18244f6c9da55486698e4e68fa6e42b
Ilya Ayzenshtok <ilya.ayzenshtok@goeuro.com>
2017-01-27 15:35:58 +0100

    clean-up

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -20,11 +20,11 @@ var ErrorModal = React.createClass({
 
     return (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-          <h4>{title}</h4>
-          <p>{message}</p>
-          <p>
-              <button className="button hollow" data-close="">Okay</button>
-          </p>
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className="button hollow" data-close="">Okay</button>
+        </p>
       </div>
       );
   }
