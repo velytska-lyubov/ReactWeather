539cef617b9ad75a7b20226447430026e0325b77
Lyubov Velytska <velytska.lyubov@gmail.com>
2017-02-05 17:39:26 +0100

    New Laptop

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,34 +1,45 @@
 var React = require('react');
+var ReactDOM = require ('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string,
-    message: React.PropTypes.string.isRequired
-  },
   getDefaultProps: function() {
     return {
       title: 'Error',
-      message: null,
     };
   },
-  componentDidMount: function() {
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
+  propTypes:{
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
   },
-  render: function() {
+  componentDidMount: function() {
     var {title, message} = this.props;
-
-    return (
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
         <p>
-          <button className="button hollow" data-close="">Okay</button>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
         </p>
       </div>
-      );
-  }
+    );
 
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+
+  render: function() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
 });
 
 module.exports = ErrorModal;
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -16,7 +16,9 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined,
     });
 
     openWeatherMap.getTemp(location).then(function(temp) {
@@ -29,9 +31,25 @@ var Weather = React.createClass({
     }, function(e) {
       that.setState({
         isLoading: false,
-        errorMessage: e.message
+        errorMessage: e.message,
       });
     });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function() {
     var {isLoading, temp, location, errorMessage} = this.state;
